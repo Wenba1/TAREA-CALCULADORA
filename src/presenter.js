b5562be7ece9fce8764683e4c3c204c6f3022e47
Wendy Balderrama <balderramawendym@gmail.com>
2024-08-13 12:38:26 -0400

    Primer cambio c:

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,22 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const sumarForm = document.querySelector("#sumar-form");
+// Eliminado: const form = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+// Añadir eventos a los botones y manejar las operaciones en el mismo form
+sumarForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  
+  if (event.submitter.id === "sumar-button") {
+    div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  } else if (event.submitter.id === "multiplicar-button") {
+    div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  }
 });
